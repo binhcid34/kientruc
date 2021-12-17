f76e86b59bc44e6f36b1f9a19ea3d31210b5be5d
binhcid34 <71257284+binhcid34@users.noreply.github.com>
2021-12-17 11:00:53 +0700

    ver 02

--- mapdata.js
+++ mapdata.js
@@ -1,312 +1,333 @@
 var simplemaps_countrymap_mapdata={
   main_settings: {
    //General settings
-    width: "600", //'700' or 'responsive'
+    width: "650", //'700' or 'responsive'
     background_color: "#FFFFFF",
     background_transparent: "yes",
     border_color: "#ffffff",
     
-    //State defaults
-    state_description: "State description",
-    state_color: "#88A4BC",
-    state_hover_color: "#3B729F",
-    state_url: "",
-    border_size: 1.5,
-    all_states_inactive: "no",
-    all_states_zoomable: "yes",
+     //State defaults
+     state_description: "",
+     state_color: "#88A4BC",
+     state_hover_color: "#3B729F",
+     state_url: "",
+     border_size: 1.5,
+     all_states_inactive: "no",
+     all_states_zoomable: "yes",
+     location_description: "",
+     location_color: "",
+     location_opacity: 0.8,
+     location_hover_opacity: 1,
+     location_size: "20",
+     location_type: "circle",
+     location_image_source: "frog.png",
+     location_border_color: "#FFFFFF",
+     location_border: 2,
+     location_hover_border: 2.5,
+     all_locations_inactive: "no",
+     all_locations_hidden: "no",
+     
+     //Label defaults
+     label_color: "#d5ddec",
+     label_hover_color: "#d5ddec",
+     label_size: 22,
+     label_font: "Arial",
+     hide_labels: "no",
+     hide_eastern_labels: "no",
     
-    //Location defaults
-    location_description: "Location description",
-    location_url: "/Documentation.html",
-    location_color: "#FF0067",
-    location_opacity: 0.8,
-    location_hover_opacity: 1,
-    location_size: 25,
-    location_type: "circle",
-    location_image_source: "frog.png",
-    location_border_color: "#FFFFFF",
-    location_border: 2,
-    location_hover_border: 2.5,
-    all_locations_inactive: "no",
-    all_locations_hidden: "no",
-    
-    //Label defaults
-    label_color: "#d5ddec",
-    label_hover_color: "#d5ddec",
-    label_size: 22,
-    label_font: "Arial",
-    hide_labels: "no",
-    hide_eastern_labels: "no",
-   
-    //Zoom settings
-    zoom: "yes",
-    manual_zoom: "yes",
-    back_image: "no",
-    initial_back: "no",
-    initial_zoom: "-1",
-    initial_zoom_solo: "no",
-    region_opacity: 1,
-    region_hover_opacity: 0.6,
-    zoom_out_incrementally: "yes",
-    zoom_percentage: 0.99,
-    zoom_time: 0.5,
-    
-    //Popup settings
-    popup_color: "white",
-    popup_opacity: 0.9,
-    popup_shadow: 1,
-    popup_corners: 5,
-    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
-    popup_nocss: "no",
-    
-    //Advanced settings
-    div: "map",
-    auto_load: "yes",
-    url_new_tab: "no",
-    images_directory: "default",
-    fade_time: 0.1,
-    link_text: "View Website",
-    popups: "detect",
-    state_image_url: "",
-    state_image_position: "",
-    location_image_url: ""
-  },
-  state_specific: {
-    VNM429: {
-      name: "Quảng Ninh"
-    },
-    VNM444: {
-      name: "Tây Ninh"
-    },
-    VNM450: {
-      name: "Điện Biên"
-    },
-    VNM451: {
-      name: "Đông Bắc"
-    },
-    VNM452: {
-      name: "Thái Nguyên"
-    },
-    VNM453: {
-      name: "Lai Chau"
-    },
-    VNM454: {
-      name: "Lạng Sơn"
-    },
-    VNM455: {
-      name: "Son La"
-    },
-    VNM456: {
-      name: "Thanh Hóa"
-    },
-    VNM457: {
-      name: "Tuyên Quang"
-    },
-    VNM458: {
-      name: "Yên Bái"
-    },
-    VNM459: {
-      name: "Hòa Bình"
-    },
-    VNM460: {
-      name: "Hải Dương"
-    },
-    VNM4600: {
-      name: "Hải Phòng"
-    },
-    VNM461: {
-      name: "Đồng Bằng Sông Hồng"
-    },
-    VNM462: {
-      name: "Ha Noi"
-    },
-    VNM463: {
-      name: "Bắc Ninh"
-    },
-    VNM464: {
-      name: "Vĩnh Phúc"
-    },
-    VNM466: {
-      name: "Ninh Bình"
-    },
-    VNM467: {
-      name: "Hà Nam"
-    },
-    VNM468: {
-      name: "Nam Định"
-    },
-    VNM469: {
-      name: "Phú Thọ"
-    },
-    VNM470: {
-      name: "Bắc Giang"
-    },
-    VNM471: {
-      name: "Thái Bình"
-    },
-    VNM474: {
-      name: "Ha Tinh"
-    },
-    VNM475: {
-      name: "Nghệ An"
-    },
-    VNM476: {
-      name: "Quảng Bình"
-    },
-    VNM477: {
-      name: "Dak Lak"
-    },
-    VNM478: {
-      name: "Gia Lai"
-    },
-    VNM479: {
-      name: "Khánh Hòa"
-    },
-    VNM480: {
-      name: "Lâm Đồng"
-    },
-    VNM481: {
-      name: "Ninh Thuận"
-    },
-    VNM482: {
-      name: "Phú Yên"
-    },
-    VNM483: {
-      name: "Bình Dương"
-    },
-    VNM4834: {
-      name: "Tiền Giang"
-    },
-    VNM4835: {
-      name: "Đắk Nông"
-    },
-    VNM484: {
-      name: "Bình Phước"
-    },
-    VNM485: {
-      name: "Bình Định"
-    },
-    VNM486: {
-      name: "Kon Tum"
-    },
-    VNM487: {
-      name: "Quàng Nam"
-    },
-    VNM488: {
-      name: "Quảng Ngãi"
-    },
-    VNM489: {
-      name: "Quảng Trị"
-    },
-    VNM490: {
-      name: "Thừa Thiên - Huế"
-    },
-    VNM491: {
-      name: "Đà Nẵng"
-    },
-    VNM495: {
-      name: "Bà Rịa - Vũng Tàu"
-    },
-    VNM496: {
-      name: "Bình Thuận"
-    },
-    VNM497: {
-      name: "Đông Nam Bộ"
-    },
-    VNM498: {
-      name: "An Giang"
-    },
-    VNM499: {
-      name: "Can Tho"
-    },
-    VNM500: {
-      name: "Ð?ng Tháp"
-    },
-    VNM501: {
-      name: "Hồ Chí Minh city"
-    },
-    VNM502: {
-      name: "Kiên Giang"
-    },
-    VNM503: {
-      name: "Long An"
-    },
-    VNM504: {
-      name: "Bến Tre"
-    },
-    VNM505: {
-      name: "Hau Giang"
-    },
-    VNM506: {
-      name: "Bạc Liêu"
-    },
-    VNM507: {
-      name: "Cà Mau"
-    },
-    VNM508: {
-      name: "Sóc Trăng"
-    },
-    VNM509: {
-      name: "Trà Vinh"
-    },
-    VNM510: {
-      name: "Vĩnh Long"
-    },
-    VNM511: {
-      name: "Cao Bằng"
-    },
-    VNM512: {
-      name: "Hà Giang"
-    },
-    VNM5483: {
-      name: "Lào Cai"
-    }
-  },
-  locations: {
-    "0": {
-      lat: "21.028167",
-      lng: "105.854152",
-      name: "Hanoi"
-    },
-    "1": {
-      lat: "21.036051125867967",
-      lng: "105.83359484191419",
-      name: "Chùa một cột"
-    },
-    "2": {
-      lat: "16.43348198654348",
-      lng: "107.56539658397996",
-      name: "Lăng Tự Đức"
-    },
-    "3": {
-      lat: "16.46941339567482",
-      lng: "107.57791185514415",
-      name: "Kinh thành Huế"
-    },
-    "4": {
-      lat: "21.049496596759248",
-      lng: "105.8370571804914",
-      name: "Chùa Trấn Quốc"
-    },
-    "5": {
-      lat: "20.284688338241125",
-      lng: "105.90531539938421",
-      name: "Đền vua Đinh Tiên Hoàng"
-    },
-    "6": {
-      lat: "21.035563975735542",
-      lng: "105.84281837707215",
-      name: "Hoàng thành Thăng Long"
-    },
-    "7": {
-      lat: "21.155656077997094",
-      lng: "105.7346712147435",
-      name: "Đền Hai Bà Trưng"
-    }
-  },
-  labels: {},
-  legend: {
-    entries: []
-  },
-  regions: {}
-};
+     //Zoom settings
+     zoom: "yes",
+     manual_zoom: "yes",
+     back_image: "no",
+     initial_back: "no",
+     initial_zoom: "-1",
+     initial_zoom_solo: "no",
+     region_opacity: 1,
+     region_hover_opacity: 0.6,
+     zoom_out_incrementally: "yes",
+     zoom_percentage: 0.99,
+     zoom_time: 0.5,
+     
+     //Popup settings
+     popup_color: "white",
+     popup_opacity: 0.9,
+     popup_shadow: 1,
+     popup_corners: 5,
+     popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+     popup_nocss: "no",
+     
+     //Advanced settings
+     div: "map",
+     auto_load: "yes",
+     url_new_tab: "no",
+     images_directory: "default",
+     fade_time: 0.1,
+     link_text: "View Website",
+     popups: "detect",
+     state_image_url: "",
+     state_image_position: "",
+     location_image_url: ""
+   },
+   state_specific: {
+     VNM429: {
+       name: "Quảng Ninh"
+     },
+     VNM444: {
+       name: "Tây Ninh"
+     },
+     VNM450: {
+       name: "Điện Biên"
+     },
+     VNM451: {
+       name: "Đông Bắc"
+     },
+     VNM452: {
+       name: "Thái Nguyên"
+     },
+     VNM453: {
+       name: "Lai Chau"
+     },
+     VNM454: {
+       name: "Lạng Sơn"
+     },
+     VNM455: {
+       name: "Son La"
+     },
+     VNM456: {
+       name: "Thanh Hóa"
+     },
+     VNM457: {
+       name: "Tuyên Quang"
+     },
+     VNM458: {
+       name: "Yên Bái"
+     },
+     VNM459: {
+       name: "Hòa Bình"
+     },
+     VNM460: {
+       name: "Hải Dương"
+     },
+     VNM4600: {
+       name: "Hải Phòng"
+     },
+     VNM461: {
+       name: "Đồng Bằng Sông Hồng"
+     },
+     VNM462: {
+       name: "Ha Noi"
+     },
+     VNM463: {
+       name: "Bắc Ninh"
+     },
+     VNM464: {
+       name: "Vĩnh Phúc"
+     },
+     VNM466: {
+       name: "Ninh Bình"
+     },
+     VNM467: {
+       name: "Hà Nam"
+     },
+     VNM468: {
+       name: "Nam Định"
+     },
+     VNM469: {
+       name: "Phú Thọ"
+     },
+     VNM470: {
+       name: "Bắc Giang"
+     },
+     VNM471: {
+       name: "Thái Bình"
+     },
+     VNM474: {
+       name: "Ha Tinh"
+     },
+     VNM475: {
+       name: "Nghệ An"
+     },
+     VNM476: {
+       name: "Quảng Bình"
+     },
+     VNM477: {
+       name: "Dak Lak"
+     },
+     VNM478: {
+       name: "Gia Lai"
+     },
+     VNM479: {
+       name: "Khánh Hòa"
+     },
+     VNM480: {
+       name: "Lâm Đồng"
+     },
+     VNM481: {
+       name: "Ninh Thuận"
+     },
+     VNM482: {
+       name: "Phú Yên"
+     },
+     VNM483: {
+       name: "Bình Dương"
+     },
+     VNM4834: {
+       name: "Tiền Giang"
+     },
+     VNM4835: {
+       name: "Đắk Nông"
+     },
+     VNM484: {
+       name: "Bình Phước"
+     },
+     VNM485: {
+       name: "Bình Định"
+     },
+     VNM486: {
+       name: "Kon Tum"
+     },
+     VNM487: {
+       name: "Quàng Nam"
+     },
+     VNM488: {
+       name: "Quảng Ngãi"
+     },
+     VNM489: {
+       name: "Quảng Trị"
+     },
+     VNM490: {
+       name: "Thừa Thiên - Huế"
+     },
+     VNM491: {
+       name: "Đà Nẵng"
+     },
+     VNM495: {
+       name: "Bà Rịa - Vũng Tàu"
+     },
+     VNM496: {
+       name: "Bình Thuận"
+     },
+     VNM497: {
+       name: "Đông Nam Bộ"
+     },
+     VNM498: {
+       name: "An Giang"
+     },
+     VNM499: {
+       name: "Can Tho"
+     },
+     VNM500: {
+       name: "Ð?ng Tháp"
+     },
+     VNM501: {
+       name: "Hồ Chí Minh city"
+     },
+     VNM502: {
+       name: "Kiên Giang"
+     },
+     VNM503: {
+       name: "Long An"
+     },
+     VNM504: {
+       name: "Bến Tre"
+     },
+     VNM505: {
+       name: "Hau Giang"
+     },
+     VNM506: {
+       name: "Bạc Liêu"
+     },
+     VNM507: {
+       name: "Cà Mau"
+     },
+     VNM508: {
+       name: "Sóc Trăng"
+     },
+     VNM509: {
+       name: "Trà Vinh"
+     },
+     VNM510: {
+       name: "Vĩnh Long"
+     },
+     VNM511: {
+       name: "Cao Bằng"
+     },
+     VNM512: {
+       name: "Hà Giang"
+     },
+     VNM5483: {
+       name: "Lào Cai"
+     }
+   },
+   locations: {
+     "0": {
+       lat: "16.43348198654348",
+       lng: "107.56539658397996",
+       name: "Lăng Tự Đức",
+       color: "#ea27ea",
+       description: "Lăng Tự Đức có kiến trúc cầu kỳ, phong cảnh sơn thủy hữu tình và là một trong những lăng tẩm đẹp nhất của hoàng gia nhà Nguyễn.",
+       url: "/mapVietNam/Documentation.html"
+     },
+     "1": {
+       lat: "21.036051125867967",
+       lng: "105.83359484191419",
+       name: "Chùa một cột",
+       color: "#44df44",
+       description: "Chùa Một Cột xây dựng từ thời nhà Lý có tên ban đầu là Liên Hoa Đài (蓮花臺) có tức là Đài Hoa Sen với lối kiến trúc độc đáo: một điện thờ đặt trên một cột trụ duy nhất.",
+       url: "/mapVietNam/Documentation.html"
+     },
+     "2": {
+       lat: "21.155656077997094",
+       lng: "105.7346712147435",
+       name: "Đền Hai Bà Trưng",
+       description: "còn gọi là Đền Đồng Nhân vốn dựng từ năm 1160 đời vua Lý Anh Tông ở phường Bố Cái, tức là bãi Đồng Nhân, bên bờ sông Hồng. ",
+       color: "#44df44",
+       url: "/mapVietNam/Documentation.html"
+     },
+     "3": {
+       lat: "21.049496596759248",
+       lng: "105.8370571804914",
+       name: "Chùa Trấn Quốc",
+       description: "Chùa Trấn Quốc nằm trên một hòn đảo phía Đông Hồ Tây (quận Tây Hồ), chùa có lịch sử gần 1500 năm, được coi là lâu đời nhất ở Thăng Long - Hà Nội. Kiến trúc chùa có sự kết hợp hài hoà giữa tính uy nghiêm, cổ kính với cảnh quan thanh nhã giữa nền tĩnh lặng của một hồ nước mênh mang. Là trung tâm Phật giáo của kinh thành Thăng Long vào thời nhà Lý và nhà Trần.",
+       color: "#44df44",
+       url: "/mapVietNam/Documentation.html"
+     },
+     "4": {
+       lat: "20.28463743086547",
+       lng: "105.90797446189045",
+       name: "Cố đô Hoa Lư",
+       description: "Hoa Lư là kinh đô đầu tiên của nhà nước phong kiến Trung ương tập quyền ở Việt Nam với các dấu ấn lịch sử: thống nhất giang sơn, đánh Tống - dẹp Chiêm và phát tích quá trình định đô Hà Nội. Năm 1010 vua Lý Thái Tổ dời kinh đô từ Hoa Lư (Ninh Bình) về Thăng Long (Hà Nội), Hoa Lư trở thành Cố đô.",
+       color: "#44df44",
+       url: "/mapVietNam/Documentation.html"
+     },
+     "5": {
+       lat: "21.035563975735542",
+       lng: "105.84281837707215",
+       name: "Hoàng thành Thăng Long",
+       color: "#daf447",
+       url: "/mapVietNam/Documentation.html",
+       description: "Hoàng thành Thăng Long (chữ Hán: 昇龍皇城; Hán-Việt: Thăng Long hoàng thành) là quần thể di tích gắn với lịch sử kinh thành Thăng Long - Đông Kinh và tỉnh thành Hà Nội bắt đầu từ thời kì tiền Thăng Long (An Nam đô hộ phủ thế kỷ VII) qua thời Đinh - Tiền Lê, phát triển mạnh dưới thời Lý, Trần, Lê và thành Hà Nội dưới triều Nguyễn."
+     },
+     "6": {
+       lat: "20.074479850197378",
+       lng: "105.6068521686884",
+       description: "Thành nhà Hồ (hay còn gọi là thành Tây Đô, thành An Tôn, thành Tây Kinh hay thành Tây Giai) là kinh đô nước Đại Ngu (quốc hiệu Việt Nam thời nhà Hồ), nằm trên địa phận nay thuộc tỉnh Thanh Hóa. Đây là tòa thành kiên cố với kiến trúc độc đáo bằng đá có quy mô lớn hiếm hoi ở Việt Nam, có giá trị và độc đáo nhất, duy nhất còn lại ở tại Đông Nam Á và là một trong rất ít những thành lũy bằng đá còn lại trên thế giới",
+       name: "Thành Nhà Hồ",
+       color: "#456BD9",
+       url: "/mapVietNam/Documentation.html"
+     },
+     "7": {
+       lat: "21.02743303165515",
+       lng: "105.835432715467",
+       name: "Văn miếu Quốc Tử Giám",
+       color: "#44df44",
+       url: "/mapVietNam/Documentation.html",
+       description: "Văn Miếu Quốc Tử Giám là quần thể di tích đa dạng và phong phú hàng đầu của thành phố Hà Nội, nằm ở phía Nam kinh thành Thăng Long. Quần thể kiến trúc Văn Miếu – Quốc Tử Giám bao gồm: hồ Văn, khu Văn Miếu – Quốc Tử Giám và vườn Giám, mà kiến trúc chủ thể là Văn Miếu - nơi thờ Khổng Tử và Quốc Tử Giám - trường đại học đầu tiên của Việt Nam"
+     }
+   },
+   labels: {},
+   legend: {
+     entries: []
+   },
+   regions: {}
+ };
